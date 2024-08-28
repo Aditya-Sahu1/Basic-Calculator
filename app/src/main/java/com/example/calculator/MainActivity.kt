db2789eb28dd735859c3af73f77d5a4fada098b3
Aditya-Sahu1 <adityaksahu84@gmail.com>
2024-08-28 16:01:57 +0530

    Updated version

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.util.Stack
+import kotlin.math.ceil
+import kotlin.math.floor
 
-private  var s:String=""
-private var answer="123"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+          var s:String=""
+         var answer="0"
 
         var b1=findViewById<Button>(R.id.btn1)
         var b2=findViewById<Button>(R.id.btn2)
@@ -30,6 +34,10 @@ class MainActivity : AppCompatActivity() {
         var disp=findViewById<TextView>(R.id.exp)
         var ans=findViewById<TextView>(R.id.ans)
         var cut=findViewById<Button>(R.id.clear)
+        var multiply=findViewById<Button>(R.id.mul)
+        var divide=findViewById<Button>(R.id.div)
+        var dot=findViewById<Button>(R.id.dec)
+        var modulos=findViewById<Button>(R.id.mod)
 
         cut.setOnClickListener(){
 
@@ -46,8 +54,15 @@ class MainActivity : AppCompatActivity() {
         ans.text=""
 
         equal.setOnClickListener(){
-           if(answer!="0") ans.text= answer
-            else ans.text=disp.text
+           if(answer!="0") {
+
+               ans.text= answer
+           }
+            else {
+                disp.text="0"
+               ans.text = disp.text
+
+           }
         }
         b0.setOnClickListener(){
             s +="0"
@@ -110,6 +125,27 @@ class MainActivity : AppCompatActivity() {
             disp.text= s
             ans.text=""
         }
+        multiply.setOnClickListener(){
+            s +="*"
+            disp.text= s
+            ans.text=""
+        }
+        divide.setOnClickListener(){
+            s +="/"
+            disp.text= s
+            ans.text=""
+        }
+        dot.setOnClickListener(){
+            s +="."
+            disp.text= s
+            ans.text=""
+        }
+        modulos.setOnClickListener(){
+            s +="%"
+            disp.text= s
+            ans.text=""
+        }
+
         open.setOnClickListener(){
             s +="("
             disp.text= s
@@ -123,84 +159,142 @@ class MainActivity : AppCompatActivity() {
         ans.setOnClickListener(){
 
             ans.text= s
+            disp.text=s
         }
 
-        fun solve(s: String): Int {
-            var ans = 0
+
+        fun precedence(op: Char): Int {
+            return when (op) {
+                '+', '-' -> 1
+                '*', '/', '%' -> 2
+                '^' -> 3
+                else -> -1
+            }
+        }
+
+//        fun applyOperation(op: Char, b: Double, a: Double): Double {
+//            return when (op) {
+//                '+' -> a + b
+//                '-' -> a - b
+//                '*' -> a * b
+//                '/' -> a / b
+//                '%' -> a % b
+//                '^' -> Math.pow(a, b)
+//                else -> throw IllegalArgumentException("Invalid operator: $op")
+//            }
+//        }
+fun applyOperation(op: Char, b: Double, a: Double): Double {
+    return when (op) {
+        '+' -> a + b
+        '-' -> a - b
+        '*' -> a * b
+        '/' -> {
+            if (b == 0.0) {
+                throw ArithmeticException("Division by zero")
+            }
+            a / b
+        }
+        '%' -> a % b
+        '^' -> Math.pow(a, b)
+        else -> throw IllegalArgumentException("Invalid operator: $op")
+    }
+}
+
+
+        fun infixToPostfix(expression: String): String {
+            val result = StringBuilder()
+            val stack = Stack<Char>()
             var i = 0
-            while (s[i] != '+' && s[i] != '-')
-                i++
-            var k = 0
-            for (p in 0 until i)
-                k = k * 10 + (s[p] - '0')
-            ans = k
-            k = 0
-            while (i < s.length - 1) {
-                var e = i + 1
-                k = 0
-                while (s[e] != '+' && s[e] != '-')
-                    e++
-                for (p in i + 1 until e)
-                    k = k * 10 + (s[p] - '0')
 
-                if (s[i] == '+')
-                    ans += k
-                else
-                    ans -= k
-                i = e
-            }
-            return ans
-        }
+            while (i < expression.length) {
+                val c = expression[i]
 
-        fun calculate(s: String): Int? {
-            val st = mutableListOf<Int>()
-            var copy = ""
-            for (i in s.indices) {
-                if (s[i] != ' ')
-                    copy += s[i]
-            }
-            var s = copy
-            copy = ""
-            for (i in s.indices) {
-                val c = s[i]
-                if (c == '(')
-                    if (i == 0)
-                        st.add(1)
-                    else if (i >= 1 && s[i - 1] == '-')
-                        if (st.size > 0)
-                            st.add(-1 * st.last())
-                        else
-                            st.add(-1)
-
-                    else if (st.size > 0)
-                        st.add(st.last())
-                    else
-                        st.add(1)
-
-                else if (c == ')')
-                    st.removeAt(st.size - 1)
-                else {
-                    var t = 0
-                    if (st.size > 0)
-                        t = st.last()
-                    if (c == '+' && t < 0)
-                        copy += '-'
-                    else if (c == '-' && t < 0)
-                        copy += '+'
-                    else
-                        copy += c
+                when {
+                    c.isWhitespace() -> i++
+                    c.isDigit() || c == '.' -> {
+                        while (i < expression.length && (expression[i].isDigit() || expression[i] == '.')) {
+                            result.append(expression[i++])
+                        }
+                        result.append(' ')
+                    }
+                    c == '(' -> stack.push(c)
+                    c == ')' -> {
+                        while (stack.isNotEmpty() && stack.peek() != '(') {
+                            result.append(stack.pop()).append(' ')
+                        }
+                        stack.pop()
+                        i++
+                    }
+                    else -> {
+                        while (stack.isNotEmpty() && precedence(c) <= precedence(stack.peek())) {
+                            result.append(stack.pop()).append(' ')
+                        }
+                        stack.push(c)
+                        i++
+                    }
                 }
             }
-            copy = "$copy+"
-            return solve(copy)
+
+            while (stack.isNotEmpty()) {
+                result.append(stack.pop()).append(' ')
+            }
+
+            return result.toString().trim()
         }
+
+        fun evaluatePostfix(postfix: String): Double {
+            val stack = Stack<Double>()
+            val tokens = postfix.split("\\s+".toRegex())
+
+            for (token in tokens) {
+                if (token.isNotEmpty()) {
+                    if (token[0].isDigit() || (token.length > 1 && token[0] == '-')) {
+                        stack.push(token.toDouble())
+                    } else {
+                        val b = stack.pop()
+                        val a = stack.pop()
+                        stack.push(applyOperation(token[0], b, a))
+                    }
+                }
+            }
+
+            return stack.pop()
+        }
+
+//        fun evaluateInfix(expression: String): Double {
+//            val postfix = infixToPostfix(expression)
+//            return evaluatePostfix(postfix)
+//        }
+        fun evaluateInfix(expression: String): Double {
+            if(expression=="")return 0.0
+            return try {
+                val postfix = infixToPostfix(expression)
+                evaluatePostfix(postfix)
+            } catch (e: ArithmeticException) {
+                disp.text = "Error"
+                0.0
+            } catch (e: IllegalArgumentException) {
+                disp.text = "Error"
+                0.0
+            } catch (e: Exception) {
+                disp.text = "Error"
+                0.0
+            }
+        }
+
 
         equal.setOnClickListener(){
 
-            answer =calculate(s).toString()
+            var  res =evaluateInfix(s)
+            if(res == res.toInt().toDouble()) answer= res.toInt().toString()
+            else answer=res.toString()
+
             ans.text= answer
-            answer =""
+            disp.text=s
+//            answer =""
             s =""
+//            ans.text= answer
+//            disp.text=s
 
         }
 
